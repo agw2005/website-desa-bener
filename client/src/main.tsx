1184cf168474ccee699a6c2334333b91996c1d2b
agw2005 <danial.agw2005@gmail.com>
2026-07-13 11:23:17 +0700

    feat(react-router): decoupled react-router router from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
 import App from "./App.tsx";
-
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 );
