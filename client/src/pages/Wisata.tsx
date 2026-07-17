5f9a7aab8d061113445769c9ef2ae9edb5bb55b8
agw2005 <danial.agw2005@gmail.com>
2026-07-17 18:20:55 +0700

    feat(Wisata.tsx): scaffold initial design for Wisata page

--- client/src/pages/Wisata.tsx
+++ client/src/pages/Wisata.tsx
@@ -1,7 +1,25 @@
 import Primitive from "../components/reusable/Primitive.tsx";
+import RoundedSection from "../components/reusable/RoundedSection.tsx";
 
 const Wisata = () => {
-  return <Primitive>Wisata</Primitive>;
+  return (
+    <Primitive>
+      <div className="flex flex-col gap-8 px-32">
+        <RoundedSection title="UMKM DESA">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
+          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
+          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
+          non consequuntur!
+        </RoundedSection>
+        <RoundedSection title="TEMPAT WISATA">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
+          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
+          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
+          non consequuntur!
+        </RoundedSection>
+      </div>
+    </Primitive>
+  );
 };
 
 export default Wisata;
