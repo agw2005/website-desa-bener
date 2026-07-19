0d7e935db85bb5d913dfdf3b4595b77ecd32637e
agw2005 <danial.agw2005@gmail.com>
2026-07-19 18:04:44 +0700

    feat(Article): added articleId and uploadDate

--- client/src/components/reusable/ArticleSection.tsx
+++ client/src/components/reusable/ArticleSection.tsx
@@ -1,25 +1,43 @@
 import type { ReactNode } from "react";
+import { dateToText } from "../../helpers/dateToText.ts";
+import { Link } from "react-router";
 
 interface ArticleSectionProps {
   title: string;
+  uploadDate: number;
+  articleId: number;
   children: ReactNode;
 }
 
-const ArticleSection = ({ title, children }: ArticleSectionProps) => {
+const ArticleSection = (
+  { title, uploadDate, articleId, children }: ArticleSectionProps,
+) => {
   return (
     <section className="flex flex-col">
-      <div className="flex flex-col gap-8 bg-amber-300 p-8 rounded-3xl">
+      <div className="flex flex-col gap-4 bg-amber-300 p-8 rounded-3xl">
         <div className="flex flex-col gap-2">
-          <img
-            src="https://picsum.photos/id/237/200"
-            alt={`Image for article ${title}`}
-            className="w-full max-h-98 object-cover rounded-xl"
-          />
-          <h2 className="font-bold text-2xl">
-            {title}
-          </h2>
+          <Link to={`${globalThis.location.pathname}/${articleId}`}>
+            <img
+              src="https://picsum.photos/id/237/200"
+              alt={`Image for article ${title}`}
+              className="w-full max-h-98 object-cover rounded-xl | transition duration-300 ease-in-out hover:brightness-75"
+            />
+          </Link>
+          <Link
+            to={`${globalThis.location.pathname}/${articleId}`}
+            className="font-bold text-2xl"
+          >
+            <h2 className="text-black hover:text-blue-900 active:text-blue-800">
+              {title}
+            </h2>
+            <h3 className="text-black hover:text-blue-900 active:text-blue-800 | text-xs">
+              {dateToText(new Date(uploadDate))}
+            </h3>
+          </Link>
         </div>
-        {children}
+        <div className="line-clamp-3 text-sm text-justify">
+          {children}
+        </div>
       </div>
     </section>
   );
--- client/src/pages/Pengumuman.tsx
+++ client/src/pages/Pengumuman.tsx
@@ -1,58 +1,22 @@
 import Primitive from "../components/reusable/Primitive.tsx";
 import ArticleSection from "../components/reusable/ArticleSection.tsx";
 
+const PLACEHOLDER_CONTENT =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione non consequuntur!";
+
 const Pengumuman = () => {
   return (
     <Primitive>
       <div className="grid grid-cols-3 px-32 gap-8">
-        <ArticleSection title="ARTIKEL">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
-          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
-          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
-          non consequuntur!
-        </ArticleSection>
-        <ArticleSection title="ARTIKEL">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
-          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
-          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
-          non consequuntur!
-        </ArticleSection>
-        <ArticleSection title="ARTIKEL">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
-          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
-          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
-          non consequuntur!
-        </ArticleSection>
-        <ArticleSection title="ARTIKEL">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
-          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
-          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
-          non consequuntur!
-        </ArticleSection>
-        <ArticleSection title="ARTIKEL">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
-          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
-          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
-          non consequuntur!
-        </ArticleSection>
-        <ArticleSection title="ARTIKEL">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
-          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
-          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
-          non consequuntur!
-        </ArticleSection>
-        <ArticleSection title="ARTIKEL">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
-          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
-          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
-          non consequuntur!
-        </ArticleSection>
-        <ArticleSection title="ARTIKEL">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat
-          itaque quae nobis voluptas, dignissimos neque, quas ab autem eaque
-          culpa. Sapiente nulla aliquam voluptatibus architecto, labore ratione
-          non consequuntur!
-        </ArticleSection>
+        {[...Array(8)].map((_, index) => (
+          <ArticleSection
+            articleId={index}
+            title="ARTIKEL"
+            uploadDate={(new Date()).getTime()}
+          >
+            {PLACEHOLDER_CONTENT}
+          </ArticleSection>
+        ))}
       </div>
     </Primitive>
   );
