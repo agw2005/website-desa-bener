cff1e84547e20f0001b0f697e8e5812473d25264
agw2005 <danial.agw2005@gmail.com>
2026-07-25 01:12:19 +0700

    feat(Login Aparatur): added jwt creation on login with frontend

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,12 +4,50 @@ import Primitive from "../components/reusable/Primitive.tsx";
 import TextInput from "../components/reusable/inputs/TextInput.tsx";
 import NumberInput from "../components/reusable/inputs/NumberInput.tsx";
 import PasswordInput from "../components/reusable/inputs/PasswordInput.tsx";
+import type { LoginInfo } from "../types/Aparatur.d.ts";
+import { useNavigate } from "react-router";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [nik, setNik] = useState("");
   const [passwordUmum, setPasswordUmum] = useState("");
   const [nama, setNama] = useState("");
   const [passwordAparatur, setPasswordAparatur] = useState("");
+  const [inputIsEmpty, setInputIsEmpty] = useState(false);
+  const [inputIsWrong, setInputIsWrong] = useState(false);
+
+  const handleAparaturLogin = async () => {
+    setInputIsEmpty(false);
+    setInputIsWrong(false);
+
+    if (nama === "" || passwordAparatur === "") {
+      setInputIsEmpty(true);
+    } else {
+      const payload: LoginInfo = { nama, kata_sandi: passwordAparatur };
+      try {
+        const response = await fetch(
+          `http://${globalThis.location.hostname}:8000/aparatur/login`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+          },
+        );
+
+        if (response.ok) {
+          const responseBody: { jwt: string } = await response.json();
+          sessionStorage.setItem("session_token", responseBody.jwt);
+          navigate("/");
+        } else {
+          setInputIsWrong(true);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
 
   return (
     <Primitive>
@@ -66,15 +104,25 @@ const Login = () => {
                 setPasswordAparatur(e.currentTarget.value)}
             />
 
-            <Button
-              className="w-max"
-              onClick={() => {
-                console.log({ nama, passwordAparatur });
-              }}
-              variant="black"
-            >
-              Login
-            </Button>
+            <div className="flex gap-4">
+              <Button
+                className="w-max"
+                onClick={handleAparaturLogin}
+                variant="black"
+              >
+                Login
+              </Button>
+              {inputIsEmpty && (
+                <div className="w-max rounded-2xl bg-red-500 px-4 py-2 text-white font-bold">
+                  Nama dan kata sandi tidak boleh kosong
+                </div>
+              )}
+              {inputIsWrong && (
+                <div className="w-max rounded-2xl bg-red-500 px-4 py-2 text-white font-bold">
+                  Nama atau kata sandi yang anda masukan salah
+                </div>
+              )}
+            </div>
           </div>
         </form>
       </div>
